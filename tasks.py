73b65d301f9b7664119e6223a1b842ca4f55d846
Amulya Panabaka <amulyapanabaka139@gmail.com>
2024-08-14 11:12:48 +0530

    task1

---
+++ tasks.py
@@ -0,0 +1,24 @@
+#a = 30
+#b = 20
+#if a < b:
+   # print("checked")
+#else:
+  #  print("unchecked")
+
+#firstValue = input("Enter value a:")
+#SecondValue = input("Enter value b:")
+#a = eval(firstValue)
+#b = eval(SecondValue)
+#if a > b:
+   # print("condition passed")
+    #print(type(a),type(b))
+#else:
+   # print("condition failed")
+
+user_name = input("Enter your name: ")
+password = input("Enter your password: ")
+if user_name == "admin" and password == "admin":
+    print("Login successful")
+else:
+     print("Login failed")
+
